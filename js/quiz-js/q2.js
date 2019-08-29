78b0647dc0e99571ad86b7a816c6118a7e9ddf9e
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-08-29 08:59:22 +0700

    add-quiz

---
+++ js/quiz-js/q2.js
@@ -0,0 +1,5 @@
+var warna = new Array('papua','nusa tenggara','maluku','sulawesi','kalimantan');
+warna.forEach(function(value, index)
+{
+    console.log('Indeks ke-' + index + ' adalah pulau ' + value + '<br/>');
+});
