b01efe08edf2eecc3026f13e8648a985d7e7737c
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-08-30 16:03:01 +0700

    finish

---
+++ js/catetan31.js
@@ -0,0 +1,5 @@
+var nama = "Jendral Soedirman";
+console.log('Panjang string "'+ nama +'" adalah '+ nama.length + 'karakter <br/>');
+
+var pasukan = "Pasukan berkata \"Kami adalah Gerilyawan\".<br/>";
+console.log(pasukan);
---
+++ js/catetan33.js
@@ -0,0 +1,3 @@
+var pesan = "saya suka baso sapi, sate kambing dan iga bakar";
+var hasil = pesan.replace("kambing", "ayam");
+console.log(hasil);
